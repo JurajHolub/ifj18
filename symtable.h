b24c6a5a7a2425bf0f34af396808e05639e34a08
Juraj Holub <xholub40@stud.fit.vutbr.cz>
2018-10-06 09:32:30 +0200

    [FEATURE] Improve proposal of symbol table.

--- symtable.h
+++ symtable.h
@@ -14,11 +14,16 @@ table_item_t hash_table[HASH_SIZE];
 typedef struct list_s {
     data_t data;
     struct list_s *next;
-};
+} list_t;
 
 typedef struct data_s {
-    ...
-    ...
+    char *data_type;    ///< Data type of symbol. For function it is return val.
+    char *name;         ///< Name of variable or indication of constant.
+    char *value;        ///< Value of constant (string, number).
+    bool init;          ///< True if variable is initialized;
+    char *fun_name;     ///< Name of function.
+    int param_cnt;      ///< Number of parameters of function.
+    char *param_type;    ///< Data types of function parameters.
 } data_t;
 
 typedef struct table_item_s {
