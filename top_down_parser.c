b9718ed3259bdd1f7bbeb5c4791181382430655b
Matej Parobek <xparob00@stud.fit.vutbr.cz>
2018-10-27 01:19:20 +0200

    Added top down parser.
    Implemented without memory cleaning. This program wasn't tested.

---
+++ top_down_parser.c
@@ -0,0 +1,559 @@
+/**
+ * @author Matej Parobek (xparob00)
+ * @file top_down_parser.c
+ * @date 2018-10-16
+ * @brief Implementation of top-down parser
+ */
+
+#include "top_down_parser.h"
+
+//symbol table used for definitions and semantic analysis in main program body
+table_item_t *global_symtable;
+
+void create_data(data_t *data, int data_type, char *id, char *value)
+{
+    char *alloc_id = NULL,
+         *alloc_value = NULL;
+    alloc_id = (char *)malloc(sizeof(char) * (strlen(id) + 1));
+    if (alloc_id == NULL)
+    {
+        fprintf(stderr, "Compiler runtime error: unable to allocate memory");
+        //TODO abbort program
+        exit(99);
+    }
+    strcpy(alloc_id, id);
+    if (value != NULL)
+    {
+        alloc_value = (char *)malloc(sizeof(char) * (strlen(value) + 1));
+        if (alloc_value == NULL)
+        {
+            fprintf(stderr, "Compiler runtime error: unable to allocate memory");
+            //TODO abbort program
+            exit(99);
+        }
+        strcpy(alloc_value, value);
+    }
+    data->data_type = data_type;
+    data->id = alloc_id;
+    data->value = alloc_value;
+    data->param_cnt = 0;
+    data->param_id = NULL;
+}
+
+
+void syntax_error(int token_type)
+{
+    //TODO hups
+    static char *string_tokens[] = {"NOT", "SUB", "ADD", "MUL", "DIV", "NOT_EQUAL", "LESS", "INTEGER", "STRING", "NIL", "LEFT_B", "RIGHT_B", "EOL", "EQUAL", "GREATER", "LESS_EQ", "GREATER_EQ", "FLOAT", "DEF", "DO", "ELSE", "END", "IF", "THEN", "WHILE", "ASSIG", "FUN", "DELIM", "VAR"};
+    if (token_type == EOF)
+    {
+        fprintf(stderr, "Unexpected end of file\n");
+    }
+    else if (token_type == EOL)
+    {
+        fprintf(stderr, "Unexpected end of line\n");
+    }
+    else
+    {
+        fprintf(stderr, "Unexpected token: %s\n", string_tokens[token_type]);
+    }
+}
+
+int parse(void)
+{
+    global_symtable = get_hash_table();
+    //TODO free resources
+    return program_list() ? 2 : 0;
+}
+
+bool program_list(void) {
+    token_t *token = get_token();
+
+    //rule <program_list> -> EOF
+    if (token->type == EOF) //TODO
+    {
+        return true;
+    }
+
+    //rule <program_list> -> <function_def> <program_list>
+    else if (token->type == DEF)
+    {
+        ret_token(token);
+        return  function_def() && program_list();
+    }
+
+    //rule <program_list> -> <statement> <program_list>
+    else if (token->type == IF || token->type == WHILE || token->type == VAR)  //TODO VAR == ID????
+    {
+        ret_token(token);
+        return statement(global_symtable) && program_list();
+    }
+    else
+    {
+        syntax_error(token->type);
+        return false;
+    }
+}
+
+bool function_def(void) {
+    token_t *token = get_token();
+
+    //rule <function_def> -> def ID(<params>
+    if (token->type == DEF)
+    {
+        if ((token = get_token())->type == VAR)
+        {
+            //if the whole rule is checked symbol table entry will be created and data inserted, but we need data
+            //from this token, which will be in the next statement overwritten, so we need to save it
+            token_t *tmp = token;
+            if ((token = get_token())->type == LEFT_B)
+            {
+                //creating symbol table entry
+                data_t *symtable_data = (data_t *) malloc(sizeof(data_t));
+                if (symtable_data == NULL)
+                {
+                    fprintf(stderr, "Compiler runtime error: unable to allocate memory");
+                    //TODO free resources
+                    exit(99);
+                }
+
+                //inserting data from token to symbol table entry
+                create_data(symtable_data, tmp->type, tmp->attribute, NULL);
+
+                //inserting symbol table entry to symbol table
+                insert(global_symtable, symtable_data);
+
+                //creating local symtable for function body definitions and semantic analysis
+                table_item_t *function_symtable = get_hash_table();
+
+
+                return params(function_symtable, symtable_data);
+            }
+        }
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+
+}
+
+bool params(table_item_t *local_symtable, data_t *symtable_data)
+{
+    token_t *token = get_token();
+
+    //rule <params> -> ) EOL <function_body>
+    if(token->type == RIGHT_B)
+    {
+        token = get_token();
+        if (token->type == EOL)
+        {
+            return function_body(local_symtable);
+        }
+    }
+
+    //rule <params> -> <param> <param_list>
+    else if (token->type == VAR)  //TODO VAR  premenovat na ID
+    {
+        return param(symtable_data, token->attribute) && param_list(local_symtable, symtable_data);
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool param_list(table_item_t *local_symtable, data_t *symtable_data)
+{
+    token_t *token = get_token();
+
+    //rule <params_list> -> ) EOL <function_body>
+    if(token->type == RIGHT_B)
+    {
+        token = get_token();
+        if (token->type == EOL)
+        {
+            return function_body(local_symtable);
+        }
+    }
+
+    //rule <param_list> -> , <param> <param_list>
+    else if (token->type == DELIM)
+    {
+        token = get_token();
+        if (token->type == VAR)
+        {
+            return param(symtable_data, token->attribute) && param_list(local_symtable, symtable_data);
+        }
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool param(data_t *symtable_data, char *param)
+{
+    //auxiliary variables to work with dynamic array
+    int param_length = strlen(param),   //inserted string length
+        param_list_length;              //allocated memory length
+
+    //no memory was allocated
+    if (symtable_data->param_id == NULL)
+    {
+            param_list_length = 0;
+    }
+    //memory was allocated
+    else
+    {
+        param_list_length = strlen(symtable_data->param_id);
+    }
+
+    //new array where fits content of old array and the new inserted parameter + \0
+    char *tmp_param_list = (char *)malloc((param_length + param_list_length + 1) * sizeof(char));
+    if (tmp_param_list == NULL)
+    {
+        fprintf(stderr, "Compiler runtime error: unable to allocate memory");
+        //TODO abbort program
+        exit(99);
+    }
+    //old array copied in new array, if there was an old array
+    if (symtable_data->param_id != NULL)
+    {
+        strcpy(tmp_param_list, symtable_data->param_id);
+        //parameters must be separated by ' '
+        tmp_param_list[param_list_length] = ' ';
+    }
+    //+1 is the extra whitespace before parameter
+    strcpy(tmp_param_list + param_list_length + 1, param);
+
+    free(symtable_data->param_id);
+    symtable_data->param_id = tmp_param_list;
+    return true;  //TODO musi to tu byt
+}
+
+bool function_body(table_item_t *local_symtable)
+{
+    token_t *token = get_token();
+
+    //rule <function-body> -> end EOL
+    if (token->type == END)
+    {
+        token = get_token();
+        if (token->type == EOL)
+        {
+            return true;
+        }
+    }
+
+    //rule <function-body> -> <stat> <function_body>
+    else if(token->type == IF || token->type == WHILE || token->type == VAR)
+    {
+        ret_token(token);
+        return statement(local_symtable) && function_body(local_symtable);
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool statement(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <statement> -> <if_statement>
+    if (token->type == IF)
+    {
+        ret_token(token);
+        return if_statement(symtable);
+    }
+
+    //rule <statement> -> <while_statement>
+    else if(token->type == WHILE)
+    {
+        ret_token(token);
+        return while_statement(symtable);
+    }
+
+    //rule <statement> -> <function_call> or <statement> -> <expression>
+    else if(token->type == VAR)
+    {
+        //TODO co je napr. a+b patri to do statement (zatial ano)
+        token_t *next_token = get_token();
+
+        //rule <statement> -> <function_call>
+        if (token->type == VAR)
+        {
+            ret_token(next_token);
+            ret_token(token);
+            return function_call(symtable);
+        }
+
+        //rule <statement> -> <expression>
+        else
+        {
+            ret_token(next_token);  //TODO ???
+            ret_token(token);
+            return (bool)parse_expression(symtable); //TODO co to vracia
+        }
+    }
+    //TODO tento else if
+    /*else if (token->type == INTEGER ||
+             token->type == STRING ||
+             token->type == FLOAT ||
+             token->type == NOT ||
+             token->type == SUB ||
+             token->type == ADD)
+    {
+        ret_token(token);
+        return (bool)parse_expression(symtable);
+    }*/
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool if_statement(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <if_statement> -> if <expression> then EOL <if_body>
+    if (token->type == IF)
+    {
+        if (parse_expression(symtable) == 0) //TODO co to vracia
+        {
+            token = get_token();
+            if (token->type == THEN)
+            {
+                token = get_token();
+                if (token->type == EOL)
+                {
+                    return if_body(symtable);
+                }
+            }
+        }
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool if_body(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <if_body> -> else EOL <else-body>
+    if (token->type == ELSE)
+    {
+        token = get_token();
+        if (token->type == EOL)
+        {
+            return else_body(symtable);
+        }
+        else
+        {
+            syntax_error(token->type);
+            return false;
+        }
+    }
+
+    //rule <if_body> -> <stat> <if_body>
+    else
+    {
+        ret_token(token);
+        return statement(symtable) && if_body(symtable);
+    }
+}
+
+bool else_body(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <else_body> -> end EOL
+    if (token->type == END)
+    {
+        token = get_token();
+        if (token->type == EOL)
+        {
+            return true;
+        }
+        else
+        {
+            syntax_error(token->type);
+            return false;
+        }
+    }
+
+    //rule <else_body> -> <stat> <else_body>
+    else
+    {
+        ret_token(token);
+        return statement(symtable) && else_body(symtable);
+    }
+}
+
+bool while_statement(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <while_statement> -> while <expression> do EOL <while_body>
+    if (token->type == WHILE)
+    {
+        if (parse_expression(symtable)) //TODO co to vracia
+        {
+            token = get_token();
+            if (token->type == DO)
+            {
+                token = get_token();
+                if (token->type == EOL)
+                {
+                    return while_body(symtable);
+                }
+            }
+        }
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool while_body(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <while_body> -> end EOL
+    if (token->type == END)
+    {
+        token = get_token();
+        if (token->type == EOL)
+        {
+            return true;
+        }
+        else
+        {
+            syntax_error(token->type);
+            return false;
+        }
+    }
+
+    //rule <while_body> -> <stat> <while_body>
+    else
+    {
+        ret_token(token);
+        return statement(symtable) && while_body(symtable);
+    }
+}
+
+bool function_call(table_item_t *symtable)
+{
+    token_t *token = get_token();
+
+    //rule <function_call> -> ID <call_params> EOL
+    if (token->type == VAR)
+    {
+        if (call_params() == true)
+        {
+            token = get_token();
+            if (token->type == EOL)
+            {
+                return true;
+            }
+        }
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool call_params(void)
+{
+    token_t *token = get_token();
+
+    //rule <call_params> -> (<call_param>)
+    if (token->type == LEFT_B)
+    {
+        if (call_param() == true)
+        {
+            token = get_token();
+            if (token->type == RIGHT_B)
+            {
+                return true;
+            }
+        }
+
+        //rule failed, bad syntax
+        //TODO free resources
+        syntax_error(token->type);
+        return false;
+    }
+
+    //rule <call_params> -> <call_param>
+    else
+    {
+        ret_token(token);
+        return call_param();
+    }
+}
+
+bool call_param(void)
+{
+    token_t *token = get_token();
+
+    //rule <call_param> -> ID <call_param_list>
+    if (token->type == VAR)
+    {
+        return call_param_list();
+    }
+
+    //rule <call_param> -> ɛ
+    else if (token->type == RIGHT_B || token->type == EOL)
+    {
+        ret_token(token);
+        return true;
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
+
+bool call_param_list(void)
+{
+    token_t *token = get_token();
+
+    //rule <call_param_list> -> , ID <call_param_list>
+    if (token->type == DELIM)
+    {
+        token = get_token();
+        if (token->type == VAR)
+        {
+            return call_param_list();
+        }
+    }
+
+    //rule <call_param_list> -> ɛ
+    else if (token->type == RIGHT_B || token->type == EOL)
+    {
+        ret_token(token);
+        return true;
+    }
+
+    //rule failed, bad syntax
+    //TODO free resources
+    syntax_error(token->type);
+    return false;
+}
---
+++ top_down_parser.h
@@ -0,0 +1,245 @@
+/**
+ * @author Matej Parobek (xparob00)
+ * @file top_down_parser.c
+ * @date 2018-10-16
+ * @brief Implementation of top-down parser
+ */
+
+#ifndef IFJ18_TOP_DOWN_PARSER_H
+#define IFJ18_TOP_DOWN_PARSER_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "expressions_parser.h"
+#include "test.h"
+#include "symtable.h"
+
+/**
+ * @brief Run syntax analysis, initialize and clean data used by analysis
+ * @return 0 correct syntax, 2 incorrect syntax
+ */
+int parse(void);
+
+/**
+ * @brief Nonterminal symbol program_list and it's syntactic rules simulation
+ *
+ * Check's syntax of the main program body
+ *
+ * <program_list> -> EOF
+ * <program_list> -> <function_def> <program_list>
+ * <program_list> -> <statement> <program_list>
+ *
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool program_list(void);
+
+
+/**
+ * @brief Nonterminal symbol function_def and it's syntactic rules simulation
+ *
+ * <function_def> -> def ID(<params>
+ *
+ * nonterminal symbols <function_def>, <params>, <params_list>, <param>, <fun-body> are set of nonterminal symbols and
+ * it's rules, that check syntactic correct function definition
+ *
+ * Important: this function allocates entry for function in symbol table and insert type function and it's name here
+ * This function allocates local symbol table for definitions in function body, too The table entry and local symbol table
+ * are given to other functions checking function definition syntax by parameter for next use
+ *
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool function_def(void);
+
+/**
+ * @brief Nonterminal symbol params and it's syntactic rules simulation
+ *
+ * <params> -> ) EOL <function_body>
+ * <params> -> <param> <param_list>
+ *
+ * nonterminal symbols <function_def>, <params>, <params_list>, <param>, <fun-body> are set of nonterminal symbols and
+ * it's rules, that check syntactic correct function definition
+ *
+ * @param symtable_data allocated entry for actual function in global symbol table used for inserting function's parameters
+ * @param local_symtable this pointer function only gives to next function for use in function function_body()
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool params(table_item_t *local_symtable, data_t *symtable_data);
+
+/**
+ * @brief Nonterminal symbol param_list and it's syntactic rules simulation
+ *
+ * <param_list> -> ) EOL <function_body>
+ * <param_list> -> , <param> <param_list>
+ *
+ * nonterminal symbols <function_def>, <params>, <params_list>, <param>, <fun-body> are set of nonterminal symbols and
+ * it's rules, that check syntactic correct function definition
+ *
+ * @param symtable_data allocated entry for actual function in global symbol table used for inserting function's parameters
+ * @param local_symtable this pointer function only gives to next function for use in function function_body()
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool param_list(table_item_t *local_symtable, data_t *symtable_data);
+
+/**
+ * @brief Nonterminal symbol param checks no syntax, only insure complicated saving of parameters in symbol table
+ *
+ * Program resize array of characters where parameters are saved and divided by 1 blank space and save here parameter,
+ * which he get from params or param_list
+ *
+ * @param symtable_data allocated entry for actual function in global symbol table used for inserting function's parameters
+ * @param param inserted function parameter
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool param(data_t *symtable_data, char *param);
+
+/**
+ * @brief Nonterminal symbol function_body and it's syntactic rules simulation
+ *
+ * <function-body> -> <stat> <fun_body>
+ * <function-body> -> end EOL
+ * @param local_symtable symbol table used by definitions in function body, it's not symbol table used in main body of program
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool function_body(table_item_t *local_symtable);
+
+
+/**
+ * @brief Nonterminal symbol statement checks no syntax, only insure complicated saving of parameters in symbol table
+ *
+ * function find out, what statement have to be on input and call rule to derive it
+ *
+ * <statement> -> <if_statement>
+ * <statement> -> <while_statement>
+ * <statement> -> <function_call>
+ * <statement> -> <expression>
+ *
+ * @param symtable symbol table used by definitions in statement
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool statement(table_item_t *symtable);
+
+/**
+ * @brief Nonterminal symbol if_statement and it's syntactic rules simulation
+ *
+ * <if_statement> -> if <expression> then EOL <if_body>
+ *
+ * nonterminal symbols <if_statement>, <if_body>, <else_body> are set of nonterminal symbols and it's rules,
+ * that check syntax of if statement
+ *
+ * @param symtable symtable symbol table used by definitions in statement and by semantic analysis
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool if_statement(table_item_t *symtable);
+
+/**
+ * @brief Nonterminal symbol if_body and it's syntactic rules simulation
+ *
+ * <if_body> -> <stat> <if_body>
+ * <if_body> -> else EOL <else-body>
+ *
+ * nonterminal symbols <if_statement>, <if_body>, <else_body> are set of nonterminal symbols and it's rules,
+ * that check syntax of if statement
+ *
+ * @param symtable symtable symbol table used by definitions in statement and by semantic analysis
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool if_body(table_item_t *symtable);
+
+/**
+ * @brief Nonterminal symbol else_body and it's syntactic rules simulation
+ *
+ * <else_body> -> <stat> <else_body>
+ * <else_body> -> end EOL
+ *
+ * nonterminal symbols <if_statement>, <if_body>, <else_body> are set of nonterminal symbols and it's rules,
+ * that check syntax of if statement
+ *
+ * @param symtable symtable symbol table used by definitions in statement and by semantic analysis
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool else_body(table_item_t *symtable);
+
+/**
+ * @brief Nonterminal symbol while_statement and it's syntactic rules simulation
+ *
+ * <while_statement> -> while <expression> do EOL <while_body>
+ *
+ * nonterminal symbols <while_statement>, <while_body> are set of nonterminal symbols and it's rules,
+ * that check syntax of while statement
+ *
+ * @param symtable symtable symbol table used by definitions in statement and by semantic analysis
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool while_statement(table_item_t *symtable);
+
+/**
+ * @brief Nonterminal symbol while_body and it's syntactic rules simulation
+ *
+ * <while_body> -> <stat> <while_body>
+ * <while_body> -> end EOL
+ *
+ * nonterminal symbols <while_statement>, <while_body> are set of nonterminal symbols and it's rules,
+ * that check syntax of while statement
+ *
+ * @param symtable symtable symbol table used by definitions in statement and by semantic analysis
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool while_body(table_item_t *symtable);
+
+
+/**
+ * @brief Nonterminal symbol function_call and it's syntactic rules simulation
+ *
+ * <function_call> -> ID <call_params> EOL
+ *
+ * nonterminal symbols <function_call>, <call_params>, <call_params_list>, <call_param> are set of nonterminal symbols
+ * and it's rules, that check syntactic correct function call
+ *
+ * @param symtable to check correct function call according to it's definition in semantic analysis
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool function_call(table_item_t *symtable);
+
+/**
+ * @brief Nonterminal symbol call_params and it's syntactic rules simulation
+ *
+ * <call_params> -> (<call_param>)
+ * <call_params> -> <call_param>
+ *
+ * nonterminal symbols <function_call>, <call_params>, <call_params_list>, <call_param> are set of nonterminal symbols
+ * and it's rules, that check syntactic correct function call
+ *
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool call_params(void);
+
+/**
+ * @brief Nonterminal symbol call_param and it's syntactic rules simulation
+ *
+ * <call_param> -> ID <call_param_list>
+ * <call_param> -> ɛ
+ *
+ * nonterminal symbols <function_call>, <call_params>, <call_params_list>, <call_param> are set of nonterminal symbols
+ * and it's rules, that check syntactic correct function call
+ *
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool call_param(void);
+
+/**
+ * @brief Nonterminal symbol call_param_list and it's syntactic rules simulation
+ *
+ * <call_param_list> -> , ID <call_param_list>
+ * <call_param_list> -> ɛ
+ *
+ * nonterminal symbols <function_call>, <call_params>, <call_params_list>, <call_param> are set of nonterminal symbols
+ * and it's rules, that check syntactic correct function call
+ *
+ * @return true if syntax of program follow this rule derived rules. Simple syntax is correct, else false.
+ */
+bool call_param_list(void);
+
+#endif //IFJ18_TOP_DOWN_PARSER_H
