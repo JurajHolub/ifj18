69fe0129dd775868165f120abf35f8d294733332
Juraj Holub <xholub40@stud.fit.vutbr.cz>
2018-11-15 16:48:50 +0100

    [TEST] Add testing main.

--- main.c
+++ main.c
@@ -12,74 +12,57 @@
 #include <stdlib.h>
 #include "semantic_parser.h"
 #include "dynamic_string.h"
-#include "scanner.h"
 #include "test.h"
 #include "gen_out.h"
 #include "list.h"
+#include "global_interface.h"
+#include "error_handle.h"
 
 int main(int argc, char **argv)
 {
-    //string_t var1 = string_create("var0");
-    //string_t tmp1 = string_create("$tmp_01"); 
-
-    //table_item_t *hash_tb = get_hash_table();
-    //data_t d;
-    //d.value = string_create(NULL);
-    //d.fun_type = VAR;
-    //d.param_cnt = 0;
-    //d.param_id = string_create(NULL);
-    //d.data_type = VAR;
-    //d.id = var1;
-    //insert(hash_tb, &d);
-
-    //string_free(d.value);
-    //d.value = string_create("10");
-    //d.data_type = INTEGER;
-    //d.id = tmp1;
-    //insert(hash_tb, &d);
-    //string_free(d.param_id);
-    //string_free(d.value);
-
-    //data_t *op1, *op2;
-
+    
+    //table_item_t* hash_tb = get_main_st();
     //create_global_frame();
 
-    //op1 = search(hash_tb, var1);
-    //op2 = search(hash_tb, tmp1);
-    //set_instruction(I_MOVE, &op1, &op2, NULL); 
-    //set_instruction(I_ADDS, NULL, NULL, NULL); 
-    //set_instruction(I_SUBS, NULL, NULL, NULL); 
-    //set_instruction(I_MULS, NULL, NULL, NULL); 
-    //set_var(&op1);
-    //set_var(&op1);
-    //set_var(&op1);
-    //set_var(&op1);
+    //data_t d;
+    //d.id = string_create("10");
+    //d.type = CONST;
+    //d.value = INTEGER;
+    //d.param_cnt = 0;
+    //insert(hash_tb, &d);
+    //data_t* var1 = search(hash_tb, d.id);
+    //string_free(d.id);
+    //add_var(&var1);
 
+    //d.id = string_create("20");
+    //d.type = CONST;
+    //d.value = INTEGER;
+    //d.param_cnt = 0;
+    //insert(hash_tb, &d);
+    //data_t* var2 = search(hash_tb, d.id);
+    //string_free(d.id);
+    //add_var(&var2);
 
+    //add_prolog_inst(I_IFJCODE18, NULL, NULL, NULL);
+    //add_prolog_inst(I_CREATEFRAME, NULL, NULL, NULL);
+    //add_prolog_inst(I_PUSHFRAME, NULL, NULL, NULL);
 
-    //create_local_frame();
-    //set_instruction(I_MOVE, &op1, &op2, NULL); 
-    //set_instruction(I_DIVS, NULL, NULL, NULL); 
-    //set_instruction(I_DIVS, NULL, NULL, NULL); 
-    //set_instruction(I_DIVS, NULL, NULL, NULL); 
-    //free_local_frame();
+    //add_instruction(I_POPFRAME, NULL, NULL, NULL);
 
-    //create_local_frame();
-    //set_instruction(I_MOVE, &op1, &op2, NULL); 
-    //set_instruction(I_DIVS, NULL, NULL, NULL); 
-    //set_instruction(I_DIVS, NULL, NULL, NULL); 
-    //set_instruction(I_DIVS, NULL, NULL, NULL); 
-    //free_local_frame();
+    create_global_frame();
+
+    add_prolog_inst(I_IFJCODE18, NULL, NULL, NULL);
+    add_prolog_inst(I_CREATEFRAME, NULL, NULL, NULL);
+    add_prolog_inst(I_PUSHFRAME, NULL, NULL, NULL);
+    
+    int ret = syntax_tests(argc, argv);
+    if (ret == SUCCESS)
+        gen_program();
+    free_global_frame();
 
     //gen_program();
-
     //free_global_frame();
+    //remove_all_st();
 
-    //string_free(var1);
-    //string_free(tmp1);
-    //
-    //destroy_hash_table(hash_tb);
-
-    return syntax_tests(argc, argv);
-    return 0;
+    return ret;
 }
