19f73aff07139184cbb08427a97bd089055d70ed
Matej Parobek <xparob00@stud.fit.vutbr.cz>
2018-11-19 00:10:59 +0100

    [ERROR]Commented line to compile project with -Werror

--- top_down_parser.c
+++ top_down_parser.c
@@ -660,7 +660,7 @@ int assignment(table_item_t *symtable, bool main_body_assig)
             //calling semantic analysis, which actualize entry in symbol table for L value, too
             if (analysis_result == SUCCESS)
             {
-                data_t *ste_ptr_Rvalue = get_expr_type();
+                //data_t *ste_ptr_Rvalue = get_expr_type();
                 //ste_Lvalue.value = ste_ptr_Rvalue->value;
 
                 analysis_result = sem_action_assig(symtable, &ste_Lvalue);
